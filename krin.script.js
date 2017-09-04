e0b63982d8662c03cc57217078f053b5f48cfdd4
Hattakorn Chaiworn <admindoonung@gmail.com>
2017-09-04 15:14:28 +0700

    Update krin.script.js

--- krin.script.js
+++ krin.script.js
@@ -1,26 +1,26 @@
-$(function () {
-  $('.carousel').carousel()
+jQuery(function () {
+  jQuery('.carousel').carousel()
 
-  $('.nav_first .nav-item').click(function (e) {
+  jQuery('.nav_first .nav-item').click(function (e) {
     e.preventDefault()
-    $('.nav-item').removeClass('active')
-    $(this).addClass('active')
+    jQuery('.nav-item').removeClass('active')
+    jQuery(this).addClass('active')
   })
   // scroll functionz
   // Hide Header on on scroll down
   var didScroll
   var delta = 5
-  var navbarHeight = $('.nav_first').height()
-  var st = $(this).scrollTop()
+  var navbarHeight = jQuery('.nav_first').height()
+  var st = jQuery(this).scrollTop()
   var lastScrollTop = st
 
   if (st == 0) {
-    $('.nav_first').removeClass('bg-white')
+    jQuery('.nav_first').removeClass('bg-white')
   }else {
-    $('.nav_first').addClass('bg-white')
+    jQuery('.nav_first').addClass('bg-white')
   }
 
-  $(window).scroll(function (event) {
+  jQuery(window).scroll(function (event) {
     didScroll = true
     return false
   })
@@ -33,26 +33,26 @@ $(function () {
   }, 100)
 
   function hasScrolled () {
-    st = $(this).scrollTop()
+    st = jQuery(this).scrollTop()
 
     if (Math.abs(lastScrollTop - st) <= delta)
       return
     if (st > lastScrollTop && st > navbarHeight) {
       // Scroll Down
-      $('.nav_first').removeClass('nav-down').addClass('nav-up')
-      if ($('.nav_first > button').attr('aria-expanded')) {
-        $('#main_menu').collapse('hide')
+      jQuery('.nav_first').removeClass('nav-down').addClass('nav-up')
+      if (jQuery('.nav_first > button').attr('aria-expanded')) {
+        jQuery('#main_menu').collapse('hide')
       }
     } else {
       // Scroll Up
-      if (st + $(window).height() < $(document).height()) {
-        $('.nav_first').removeClass('nav-up').addClass('nav-down')
+      if (st + jQuery(window).height() < jQuery(document).height()) {
+        jQuery('.nav_first').removeClass('nav-up').addClass('nav-down')
       }
     }
     if (st == 0) {
-      $('.nav_first').removeClass('bg-white')
+      jQuery('.nav_first').removeClass('bg-white')
     }else {
-      $('.nav_first').addClass('bg-white')
+      jQuery('.nav_first').addClass('bg-white')
     }
     lastScrollTop = st
   }
